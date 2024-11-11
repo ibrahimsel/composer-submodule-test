579e5bf3be457431ffdfb5a178343199921e6aa5
Ibrahim Sel <ibrahim.sel@eteration.com>
2024-11-11 16:06:10 +0300

    added launch file

---
+++ revs_everywhere/launch/revs.launch.py
@@ -0,0 +1,14 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # Agent
+    node_agent = Node(
+        name="revs_everywhere",
+        package="revs_everywhere",
+        executable="revs_everywhere",
+    )
+    ld = LaunchDescription()
+    ld.add_action(node_agent)
+    return ld
