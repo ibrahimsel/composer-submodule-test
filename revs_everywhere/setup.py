4bc7ec8fd93869a1cfa0796b8635df1c86a7dac1
Ibrahim Sel <ibrahim.sel@eteration.com>
2024-11-11 14:27:25 +0300

    added config file

--- revs_everywhere/setup.py
+++ revs_everywhere/setup.py
@@ -1,26 +1,26 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
-package_name = 'revs_everywhere'
+package_name = "revs_everywhere"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='sel',
-    maintainer_email='ibrahim.sel@eteration.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="sel",
+    maintainer_email="ibrahim.sel@eteration.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'revs_everywere = revs_everywhere.revs_everywere:main'
-        ],
+        "console_scripts": ["revs_everywere = revs_everywhere.revs_everywere:main"],
     },
 )
